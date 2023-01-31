7e2a865762e9c2b113ca198c0d98bfbd7fe13c03
raoufgr <raouf.grera@gmail.com>
2023-02-01 00:16:57 +0200

    A

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,12 +1,5 @@
 const projectsData = [
-  {
-    title: 'Libya CV ',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
+
   {
     title: 'Postal addressing project',
     description: `The Libya postal addressing project was developed better than the Dubai application using the best technologies and standards.`,
